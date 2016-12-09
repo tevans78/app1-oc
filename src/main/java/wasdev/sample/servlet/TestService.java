6f30b4d70bddd6af81f4792e1c6112416f3e87a3
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2016-12-09 15:49:49 +0100

    inject

---
+++ src/main/java/wasdev/sample/servlet/TestService.java
@@ -0,0 +1,10 @@
+package wasdev.sample.servlet;
+
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class TestService {
+	public String getString() {
+		return "ploum";
+	}
+}
